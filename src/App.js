a4a744e3e0c5fcd9812959d42f6931df68404bc6
nuno <ngplbc@gmail.com>
2020-03-12 10:48:50 +0000

    filter self controlled

--- src/App.js
+++ src/App.js
@@ -7,23 +7,35 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            trips: {},
+            trips: [],
+            lat: "",
+            lng: "",
         };
     }
 
     async componentDidMount() {
         const { trips } = await getTripsHelper();
-        // const { places } = await getPlacesHelper({lat: "38.747098", lng: "-9.239545"}, 500, "lodging");
-        this.setState({ trips });
+        // get the most recent coordinates from the first truck in the array
+        const { lat, lng } = trips[0][1][0]; 
+        this.setState({ trips, lat, lng });
     }
+    
+    
+    handleOnSubmit = async (truckPlate, type, radius) => {
+        const { trips } = this.state;
+        // get most recent coordinates of specific truck
+        const { lat, lng } = trips.find(trip => trip[0] === truckPlate)[1][0];
+        // const { places } = await getPlacesHelper({ lat, lng }, type, radius);
+        this.setState({ lat, lng });
+    };  
 
     render() {
-        console.log(this.state.trips);
-        const { trips } = this.state;
+        console.log(this.state);
+        const { trips, lat, lng } = this.state;
 
         return (
             <div className="App">
-                <GoogleMapContainer trips={trips} />
+                {trips && <GoogleMapContainer trips={trips} lat={lat} lng={lng} onSubmit={this.handleOnSubmit} />}
             </div>
         );
     }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Filter.css";
 
-const Filter = ({ trips }) => {
+const Filter = ({ trips, onSubmit }) => {
+    const [truckPlate, setTruckPlate] = useState("");
+    const [type, setType] = useState("");
+    const [radius, setRadius] = useState("");
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if (!truckPlate.length || !type.length || !radius.length) {
+            // TODO: convert to modal
+            alert("All fields are required. \nPlease fill them all before submiting.");
+        } else {
+            onSubmit(truckPlate, type, radius);
+        }
+    };
 
     return (
-        <div className="formContainer">
-            <form onSubmit={(e) => e.preventDefault()}>
-                <select defaultValue={"Select license plate"}>
-                    <option disabled>Select license plate</option>
-                    {/* render option per plate */}
-                    {Object.keys(trips).map(plate => <option key={plate} value={plate}>{plate}</option>)}
-                </select>
+        <form onSubmit={handleOnSubmit}>
+            <select onChange={(e) => setTruckPlate(e.target.value)} defaultValue={"Select license plate"}>
+                <option disabled>Select license plate</option>
+                {/* render option per plate, plate is in the 1st idx */}
+                {trips.map(el => <option key={el[0]} value={el[0]}>{el[0]}</option>)}
+            </select>
 
-                <select defaultValue={"Select POI type"}>
-                    <option disabled>Select POI type</option>
-                    <option value="gas_station">Gas Stations</option>
-                    <option value="restaurant">Restaurants</option>
-                    <option value="lodging">Hotels</option>
-                </select>
+            <select onChange={(e) => setType(e.target.value)} defaultValue={"Select POI type"}>
+                <option disabled>Select POI type</option>
+                <option value="gas_station">Gas Stations</option>
+                <option value="restaurant">Restaurants</option>
+                <option value="lodging">Hotels</option>
+            </select>
 
-                <select defaultValue={"Select radius"}>
-                    <option disabled>Select radius</option>
-                    <option value="500">500m</option>
-                    <option value="1000">1Km</option>
-                    <option value="2000">2Km</option>
-                </select>
+            <select onChange={(e) => setRadius(e.target.value)} defaultValue={"Select radius"}>
+                <option disabled>Select radius</option>
+                <option value="500">500m</option>
+                <option value="1000">1Km</option>
+                <option value="2000">2Km</option>
+            </select>
 
-                <input type="submit" value="Apply"/>
-            </form>
-        </div>
+            <input type="submit" value="Apply"/>
+        </form>
     );
 };
 
