73e73865f144f4e5229f998bb734a510718cdd75
nuno <ngplbc@gmail.com>
2020-03-14 01:59:47 +0000

    add responsiveness

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -17,29 +17,31 @@ const Filter = ({ trips, onSubmit }) => {
     };
 
     return (
-        <form onSubmit={handleOnSubmit}>
-            <select onChange={(e) => setTruckPlate(e.target.value)} defaultValue={"Select license plate"}>
-                <option disabled>Select license plate</option>
-                {/* render option per plate, plate is in the 1st idx */}
-                {trips.map(el => <option key={el[0]} value={el[0]}>{el[0]}</option>)}
-            </select>
+        <div className="formContainer">
+            <form onSubmit={handleOnSubmit}>
+                <select onChange={(e) => setTruckPlate(e.target.value)} defaultValue={"Select license plate"}>
+                    <option disabled>Select license plate</option>
+                    {/* render option per plate, plate is in the 1st idx */}
+                    {trips.map(el => <option key={el[0]} value={el[0]}>{el[0]}</option>)}
+                </select>
 
-            <select onChange={(e) => setType(e.target.value)} defaultValue={"Select POI type"}>
-                <option disabled>Select POI type</option>
-                <option value="gas_station">Gas Stations</option>
-                <option value="restaurant">Restaurants</option>
-                <option value="lodging">Hotels</option>
-            </select>
+                <select onChange={(e) => setType(e.target.value)} defaultValue={"Select POI type"}>
+                    <option disabled>Select POI type</option>
+                    <option value="gas_station">Gas Stations</option>
+                    <option value="restaurant">Restaurants</option>
+                    <option value="lodging">Hotels</option>
+                </select>
 
-            <select onChange={(e) => setRadius(e.target.value)} defaultValue={"Select radius"}>
-                <option disabled>Select radius</option>
-                <option value="500">500m</option>
-                <option value="1000">1Km</option>
-                <option value="2000">2Km</option>
-            </select>
+                <select onChange={(e) => setRadius(e.target.value)} defaultValue={"Select radius"}>
+                    <option disabled>Select radius</option>
+                    <option value="500">500m</option>
+                    <option value="1000">1Km</option>
+                    <option value="2000">2Km</option>
+                </select>
 
-            <input type="submit" value="Apply"/>
-        </form>
+                <input type="submit" value="Apply"/>
+            </form>
+        </div>
     );
 };
 
