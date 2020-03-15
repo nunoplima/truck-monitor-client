5fb3522e6b9c7791f90c77ca9ae3e1cd4a84800e
nuno <ngplbc@gmail.com>
2020-03-15 01:36:19 +0000

    api url

--- src/util/tripsHelper.js
+++ src/util/tripsHelper.js
@@ -11,7 +11,7 @@ const getTrips = async () => {
         process.env.NODE_ENV === "development"
             ? "http://localhost:4000"
             : "https://truck-monitor-server.herokuapp.com";
-    const rawResponse = await fetch(`${url}/all`, options);
+    const rawResponse = await fetch(`${url}/trips`, options);
     const response = await rawResponse.json();
     return response;
 };
